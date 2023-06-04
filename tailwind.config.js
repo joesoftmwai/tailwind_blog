efa02ed5c1c1ecbc8e4a24f438e139115d91a3ae
Mwai Joseph <joesoftmwai@gmail.com>
2023-06-04 05:12:01 +0300

    init setup: google fonts, fontawesome icons

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins'],
+        'roboto': ['Roboto'],
+      }
+    },
+  },
+  plugins: [],
+}
